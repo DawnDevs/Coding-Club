c706b460660f7393a25ed19445b8a3b4862600fb
vishalmet <vishalmet003@gmail.com>
2024-05-06 19:33:38 +0530

    dashboard

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import Navbar from "../components/Navbar";
 import AttendancePic from "./assets/attendance.png";
 import DashboardPic from "./assets/dashboard.png";
 import ReportPic from "./assets/report.png";
-import Logout from './assets/logout.png'
-import Attendance from "./components/AttendanceCard";
+import Logout from "./assets/logout.png";
 import AttendanceSheet from "./AttendanceSheet";
 import CurrentDayAtt from "./components/CurrentDayAtt";
 import Reports from "./components/Reports";
 import Resources from "./UploadResource";
 import Updates from "./UploadUpdates";
-import Testimonials from './UploadTestimonials'
+import Testimonials from "./UploadTestimonials";
 
 const Dashboard = () => {
   const [selectedComponent, setSelectedComponent] = useState(null);
@@ -24,7 +22,6 @@ const Dashboard = () => {
     navigate("/");
   };
 
-
   const handleComponentClick = (component) => {
     setSelectedComponent(component);
   };
@@ -32,88 +29,124 @@ const Dashboard = () => {
   return (
     <div className="bg-cyan-50 bg-cover h-screen bricolage-font flex">
       <div className="">
-        {/* <Navbar /> */}
         <div className="flex flex-col bg-cyan-100 shadow-xl p-6 w-fit h-screen text-lg font-medium">
-          <p className=" text-2xl font-semibold">Admin Dashboard</p>
-          <div className=" pt-5">
-            <p className="  text-slate-500">MARK</p>
-            <div className="  items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("AttendanceSheet")}
-              >
-              <img className=" h-6" src={AttendancePic} alt="" />
-
-                Attendance Sheet
-              </button>
-            </div>
-          </div>
-
-          <div className=" pt-2">
-            <p className="  text-slate-500">ANALYZE</p>
-            <div className=" items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("CurrentDayAtt")}
-              >
-                <img className=" h-6" src={DashboardPic} alt="" />
-                Dashboard
-              </button>
-            </div>
-            <div className=" flex space-x-2 items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("Reports")}
-              >
-                <img className=" h-6" src={ReportPic} alt="" />
-                Report
-              </button>
-            </div>
-          </div>
-
-          <div className=" pt-2">
-            <p className="  text-slate-500">UPLOADS</p>
-            <div className=" flex space-x-2 items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("Resources")}
-              >
-                <img className=" h-6" src={ReportPic} alt="" />
-                Resources
-              </button>
-            </div>
-            <div className=" flex space-x-2 items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("Updates")}
-              >
-                <img className=" h-6" src={DashboardPic} alt="" />
-                Updates
-              </button>
-            </div>
-            <div className=" flex space-x-2 items-center hover:bg-slate-300 px-4 py-2 rounded">
-              <button
-                className=" flex gap-2 "
-                onClick={() => handleComponentClick("Testimonials")}
-              >
-                <img className=" h-6" src={ReportPic} alt="" />
-                Testimonials
-              </button>
-            </div>
-          </div>
-          <div className=" flex mt-4 space-x-2 items-center hover:bg-slate-300 px-4 py-2 rounded">
-            <button
-                className=" flex gap-2 "
-                onClick={handleLogout}
+          <p className="text-2xl font-semibold">Admin Dashboard</p>
+          <div className="">
+            <p className="text-slate-500">MARK</p>
+            <div
+              className={`rounded ${
+                selectedComponent === "AttendanceSheet" ? "bg-cyan-300" : ""
+              }`}
             >
-                <img className=" h-6" src={Logout} alt="" />
+              <div className="items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2 "
+                  onClick={() => handleComponentClick("AttendanceSheet")}
+                >
+                  <img className="h-6" src={AttendancePic} alt="" />
+                  Attendance Sheet
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className="text-slate-500">ANALYZE</p>
+            <div
+              className={`rounded ${
+                selectedComponent === "CurrentDayAtt" ? "bg-cyan-300" : ""
+              }`}
+            >
+              <div className="items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2"
+                  onClick={() => handleComponentClick("CurrentDayAtt")}
+                >
+                  <img className="h-6" src={DashboardPic} alt="" />
+                  Dashboard
+                </button>
+              </div>
+            </div>
+            <div
+              className={`rounded ${
+                selectedComponent === "Reports" ? "bg-cyan-300" : ""
+              }`}
+            >
+              <div className="flex space-x-2 items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2"
+                  onClick={() => handleComponentClick("Reports")}
+                >
+                  <img className="h-6" src={ReportPic} alt="" />
+                  Report
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className="text-slate-500">UPLOADS</p>
+            <div
+              className={`rounded ${
+                selectedComponent === "Resources" ? "bg-cyan-300" : ""
+              }`}
+            >
+              <div className="flex space-x-2 items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2"
+                  onClick={() => handleComponentClick("Resources")}
+                >
+                  <img className="h-6" src={ReportPic} alt="" />
+                  Resources
+                </button>
+              </div>
+            </div>
+            <div
+              className={`rounded ${
+                selectedComponent === "Updates" ? "bg-cyan-300" : ""
+              }`}
+            >
+              <div className="flex space-x-2 items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2"
+                  onClick={() => handleComponentClick("Updates")}
+                >
+                  <img className="h-6" src={DashboardPic} alt="" />
+                  Updates
+                </button>
+              </div>
+            </div>
+            <div
+              className={`rounded ${
+                selectedComponent === "Testimonials" ? "bg-cyan-300" : ""
+              }`}
+            >
+              <div className="flex space-x-2 items-center hover:bg-cyan-300 px-4 py-2 rounded">
+                <button
+                  className="flex gap-2"
+                  onClick={() => handleComponentClick("Testimonials")}
+                >
+                  <img className="h-6" src={ReportPic} alt="" />
+                  Testimonials
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className={`rounded ${
+              selectedComponent === "Logout" ? "bg-cyan-300" : ""
+            }`}
+          >
+            <div className="flex mt-4 space-x-2 items-center hover:bg-cyan-300 px-4 py-2 rounded">
+              <button className="flex gap-2" onClick={handleLogout}>
+                <img className="h-6" src={Logout} alt="" />
                 Logout
-            </button>
+              </button>
+            </div>
           </div>
         </div>
       </div>
       <div className="flex-grow">
-        {/* {selectedComponent === "Attendance" && <Attendance />} */}
         {selectedComponent === "AttendanceSheet" && <AttendanceSheet />}
         {selectedComponent === "CurrentDayAtt" && <CurrentDayAtt />}
         {selectedComponent === "Reports" && <Reports />}
--- frontend/src/pages/admin/components/AttendanceCard.jsx
+++ frontend/src/pages/admin/components/AttendanceCard.jsx
@@ -1,18 +1,51 @@
 // AttendanceCard.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const AttendanceCard = ({ studentData }) => {
+  const [present, setPresent] = useState(false);
+  const [absent, setAbsent] = useState(false);
+
   if (!studentData) {
     return null; // Return null if studentData is undefined or null
   }
 
+  const handlePresentChange = () => {
+    setPresent(!present);
+    if (!present) {
+      setAbsent(false); // Ensure only one checkbox is checked
+    }
+  };
+
+  const handleAbsentChange = () => {
+    setAbsent(!absent);
+    if (!absent) {
+      setPresent(false); // Ensure only one checkbox is checked
+    }
+  };
+
   return (
-    <tr className=''>
+    <tr>
       <td className="border border-gray-400 px-4 py-2">{studentData.id}</td>
       <td className="border border-gray-400 px-4 py-2">{studentData.name}</td>
-      <td className="flex justify-center space-x-16 px-4 py-2">
-        <button className="bg-green-500 hover:bg-green-400 p-2 px-4 rounded">Present </button>
-        <button className="bg-red-500 hover:bg-red-400 p-2 px-4 rounded">Absent ✖️</button>
+      <td className="flex justify-center space-x-4 px-4 py-2">
+        <label className=''>
+          <input
+            type="checkbox"
+            checked={present}
+            onChange={handlePresentChange}
+            className="form-checkbox h-5 w-5 text-green-500"
+          />
+          <span className="ml-2 text-green-500">Present</span>
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={absent}
+            onChange={handleAbsentChange}
+            className="form-checkbox h-5 w-5 text-red-500"
+          />
+          <span className="ml-2 text-red-500">Absent</span>
+        </label>
       </td>
     </tr>
   );
