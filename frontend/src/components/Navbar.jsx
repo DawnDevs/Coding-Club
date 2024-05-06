feedd9083610beb6dd742907ba7fcba36517f129
vishalmet <vishalmet003@gmail.com>
2024-05-06 19:42:27 +0530

    breadcrumps

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,24 +7,9 @@ const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
 
-
   const handleMenuClick = (menu) => {
     setActiveMenu(menu === activeMenu ? null : menu);
   };
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeMenu = () => {
-    setIsOpen(false);
-  };
-
-  const handleCloseClick = () => {
-    setIsOpen(false); // Close the sidebar when the close button is clicked
-  };
-  
 
   return (
     <div className="bg-transparent rounded-full border border-gray-900 mx-1 md:mx-10 lg:mx-32 flex-wrap shadow-xl bricolage-font">
@@ -37,121 +22,58 @@ const Navbar = () => {
           />
           <p className=" text-xl font-semibold">Anonymous</p>
         </Link>
-        {/* Mobile */}
-        <div className="lg:hidden">
-          <button
-            onClick={toggleMenu}
-            className="block text-xl font-semibold focus:outline-none mr-4"
-          >
-            ☰
-          </button>
-        </div>
-        <div
-          className={`lg:hidden absolute inset-y-0 z-50 left-0 transform transition duration-300 ease-in-out bg-white text-black backdrop-filter backdrop-blur-lg h-screen w-64 ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          }`}
-        >
-          <button
-            onClick={handleCloseClick}
-            className="absolute top-0 right-0 m-4 text-2xl focus:outline-none"
-          >
-            ❌
-          </button>
-          <ul className="px-4 mt-16 space-y-2 poppins-font font-medium">
-            <li>
-              <Link
-                to="/"
-                className="py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/experience"
-                className="py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"
-              >
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/works"
-                className="py-1 px-1 mx-1 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"
-              >
-                My Works
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/contact"
-                className="py-1 px-1 mx-1 pt-2 md:py-3 md:px-3 md:mx-3 flex-wrap hover:bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 hover:rounded-md cursor-pointer hover:text-black"
-              >
-                Contact
-              </Link>
-            </li>
-            <div className="">
-              <Link
-                activeClassName="active"
-                onMouseEnter={() => handleMenuClick("signup")}
-              >
-                <Connect
-                  setUserEmail={setUserEmail}
-                  className=" border-2 border-gray-900 shadow-lg p-2 px-4 rounded-full w-fit font-semibold bg-gradient-to-tr from-cyan-400 via-cyan-200 to-cyan-100 hover:from-cyan-100 hover:via-cyan-200 hover:to-cyan-400 hover:scale-95"
-                >
-                  Sign up 🡥
-                </Connect>
-              </Link>
-            </div>
-          </ul>
-        </div>
-        {/* Desktop */}
 
         <ul className="hidden lg:flex justify-center font-medium text-base space-x-6 ">
+          <li>
             <Link
               to="/"
-              className={" px-4 p-2 hover:cursor-pointer hover:shadow-xl border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out `menu-item` "}
-              activeClassName="active"
+              className={`px-4 p-2 hover:cursor-pointer border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out ${activeMenu === "home" ? 'shadow-xl' : ''}`}
               onMouseEnter={() => handleMenuClick("home")}
             >
               Home
             </Link>
+          </li>
+          <li>
             <Link
               to="/resources"
-              className={"px-4 p-2 hover:cursor-pointer hover:shadow-xl border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out `menu-item`"}
-              activeClassName="active"
+              className={`px-4 p-2 hover:cursor-pointer border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out ${activeMenu === "resources" ? 'shadow-xl' : ''}`}
               onMouseEnter={() => handleMenuClick("resources")}
             >
               Resources
             </Link>
+          </li>
+          <li>
             <Link
               to="/dashboard"
-              className={"px-4 p-2 hover:cursor-pointer hover:shadow-xl border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out `menu-item`"}
-              activeClassName="active"
+              className={`px-4 p-2 hover:cursor-pointer border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out ${activeMenu === "dashboard" ? 'shadow-xl' : ''}`}
               onMouseEnter={() => handleMenuClick("dashboard")}
             >
               Dashboard
             </Link>
+          </li>
+          <li>
             <Link
               to="/updates"
-              className={"px-4 p-2 hover:cursor-pointer hover:shadow-xl border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out `menu-item`"}
-              activeClassName="active"
+              className={`px-4 p-2 hover:cursor-pointer border border-gray-400 rounded-full transition-transform hover:scale-95 ease-in-out ${activeMenu === "updates" ? 'shadow-xl' : ''}`}
               onMouseEnter={() => handleMenuClick("updates")}
             >
               Updates
             </Link>
+          </li>
         </ul>
-          <Link className="hidden lg:flex hover:cursor-pointer transition-transform hover:scale-95"
-            activeClassName="active"
-            onMouseEnter={() => handleMenuClick("signup")}
+
+        <Link
+          className="hidden lg:flex hover:cursor-pointer transition-transform hover:scale-95"
+          activeClassName="active"
+          onMouseEnter={() => handleMenuClick("signup")}
+        >
+          <Connect
+            setUserEmail={setUserEmail}
+            className=" border-2 border-gray-900 shadow-lg p-2 px-4 rounded-full w-fit font-semibold bg-gradient-to-tr from-cyan-400 via-cyan-200 to-cyan-100 hover:from-cyan-100 hover:via-cyan-200 hover:to-cyan-400 hover:scale-95"
           >
-            <Connect
-              setUserEmail={setUserEmail}
-              className=" border-2 border-gray-900 shadow-lg p-2 px-4 rounded-full w-fit font-semibold bg-gradient-to-tr from-cyan-400 via-cyan-200 to-cyan-100 hover:from-cyan-100 hover:via-cyan-200 hover:to-cyan-400 hover:scale-95"
-            >
-              Sign up 🡥
-            </Connect>
-          </Link>
+            Sign up 🡥
+          </Connect>
+        </Link>
       </div>
     </div>
   );
